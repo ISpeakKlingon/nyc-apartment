bf35855ead6bec027e6e148678b9ef6265edf734
ISpeakKlingon <77762797+ISpeakKlingon@users.noreply.github.com>
2021-02-05 22:18:55 -0800

    added some ambient light

--- NYC Apartment/Assets/Plugins/upload.bat
+++ NYC Apartment/Assets/Plugins/upload.bat
@@ -1,2 +1,2 @@
 echo off
-curl -v -b cookie -X PUT -F "space_template[zip]=@F:\Unity Project Media\AltSpace\Testing\NYC Apartment\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1638086072057463233.json
+curl -v -b cookie -X PUT -F "space_template[zip]=@F:\Unity Project Media\GitHub Repository\nyc-apartment\NYC Apartment\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1638086072057463233.json
